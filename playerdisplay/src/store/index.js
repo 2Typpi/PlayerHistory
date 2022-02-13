21b7732eecee9855d552730109511847565f4e39
2Typpi <wrstle@web.de>
2022-02-13 16:33:18 +0100

    feat(api): create get for all Players (CORS Error)

--- playerdisplay/src/store/index.js
+++ playerdisplay/src/store/index.js
@@ -1,12 +1,22 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://localhost:3001/";
 
 export default createStore({
   state: {
+    players: [],
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    async loadAllPlayers() {
+      axios
+        .get("stats/players")
+        .then((response) => {
+          this.players = response;
+        })
+        .catch((err) => console.log(err));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- playerdisplay/src/views/Home.vue
+++ playerdisplay/src/views/Home.vue
@@ -5,9 +5,19 @@
 </template>
 
 <script>
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      players: Array,
+    }
+  },
   components: {
+  },
+  async created() {
+    this.players = await this.$store.dispatch("loadAllPlayers");
+    console.log(this.players);
   }
 }
 </script>
