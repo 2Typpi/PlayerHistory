a04e5d640b948b4578848fa69412d0dd1b6f1fa8
2Typpi <wrstle@web.de>
2022-02-15 18:20:52 +0100

    feat(Home): Add Table, SearchBar Component (non functional)

---
+++ playerdisplay/src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="topnav">
+    <n-input :value="inputText" inverted type="text" placeholder="Search..."/>
+  </div>
+</template>
+
+<script>
+import { NInput } from "naive-ui";
+
+export default {
+  name: "SearchBar",
+  components: {
+    NInput,
+  },
+  setup() {
+    return {
+      value: "",
+    };
+  },
+};
+</script>
+<style>
+
+</style>
--- playerdisplay/src/views/Home.vue
+++ playerdisplay/src/views/Home.vue
@@ -1,27 +1,66 @@
 <template>
   <div class="home">
-    <ul>
-      <li v-for="player in this.$data.players" :key="player.player_id">
-        {{ player.FirstName }}
-      </li>
-    </ul>
+    <n-space vertical size="large">
+      <n-layout-header>
+        <h1>Player List</h1>
+      </n-layout-header>
+      <n-layout-content content-style="padding: 24px;">
+        <div class="table-container">
+          <search-bar></search-bar>
+          <n-data-table 
+            size="large"
+            :columns="columns"
+            bordered="false"
+            :data="players"></n-data-table>
+        </div>
+      </n-layout-content>
+    </n-space>
   </div>
 </template>
 
 <script>
+import SearchBar from "../components/SearchBar.vue";
+
+import { NDataTable } from "naive-ui";
+
+const createColumns = [
+  {
+    title: "First Name",
+    key: "FirstName",
+  },
+  {
+    title: "Last Name",
+    key: "LastName",
+  },
+  {
+    title: "Games Played",
+    key: "Games",
+  },
+];
+
+
 export default {
   name: 'Home',
   data() {
     return {
       players: [],
+      columns: createColumns,
     }
   },
   components: {
+    NDataTable,
+    SearchBar,
   },
   async created() {
     await this.$store.dispatch("loadAllPlayers");
     this.players = this.$store.getters.players;
-    console.log(this.players);
   }
 }
 </script>
+
+<style>
+.table-container {
+  margin: 0 40px 0 40px;
+}
+
+</style>
